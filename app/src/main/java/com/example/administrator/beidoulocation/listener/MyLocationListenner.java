bc1a21e75cb8e2987f45bbdecebd7c5b68d57825
ake <7473276@qq.com>
2017-04-07 22:44:23 +0800

    添加定位,坐标转换

---
+++ app/src/main/java/com/example/administrator/beidoulocation/listener/MyLocationListenner.java
@@ -0,0 +1,51 @@
+package com.example.administrator.beidoulocation.listener;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+/**
+ * Created by 瑜哥 on 2017/4/7.
+ */
+
+public class MyLocationListenner implements BDLocationListener {
+    MapView mMapView;
+    BaiduMap mBaiduMap;
+    private boolean isFirstLoc=false;
+
+    public MyLocationListenner(MapView mMapView, BaiduMap mBaiduMap) {
+        this.mMapView = mMapView;
+        this.mBaiduMap = mBaiduMap;
+    }
+
+    @Override
+    public void onReceiveLocation(BDLocation location) {
+        // map view 销毁后不在处理新接收的位置
+        if (location == null || mMapView == null) {
+            return;
+        }
+        MyLocationData locData = new MyLocationData.Builder()
+                .accuracy(location.getRadius())
+                // 此处设置开发者获取到的方向信息，顺时针0-360
+                .direction(100).latitude(location.getLatitude())
+                .longitude(location.getLongitude()).build();
+        mBaiduMap.setMyLocationData(locData);
+        if (isFirstLoc) {
+            isFirstLoc = false;
+            LatLng ll = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+            MapStatus.Builder builder = new MapStatus.Builder();
+            builder.target(ll).zoom(18.0f);
+            mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+        }
+    }
+
+    public void onReceivePoi(BDLocation poiLocation) {
+
+    }
+}
--- app/src/main/java/com/example/administrator/beidoulocation/trrain/TrrainFragment.java
+++ app/src/main/java/com/example/administrator/beidoulocation/trrain/TrrainFragment.java
@@ -2,10 +2,25 @@ package com.example.administrator.beidoulocation.trrain;
 
 
 import android.view.View;
+import android.widget.Toast;
 
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 import com.example.administrator.beidoulocation.R;
+import com.example.administrator.beidoulocation.listener.MyLocationListenner;
 import com.example.administrator.beidoulocation.mvp.MVPBaseFragment;
 
 /**
@@ -14,10 +29,11 @@ import com.example.administrator.beidoulocation.mvp.MVPBaseFragment;
  */
 
 public class TrrainFragment extends MVPBaseFragment<TrrainContract.View, TrrainPresenter>
-        implements TrrainContract.View {
-
+        implements TrrainContract.View, OnGetGeoCoderResultListener {
+    GeoCoder mSearch = null;
     private MapView mMapView;
     private BaiduMap mBaiduMap;
+    private LocationClient mLocClient;
 
     @Override
    public int getLayoutResId() {
@@ -29,16 +45,33 @@ public class TrrainFragment extends MVPBaseFragment<TrrainContract.View, TrrainP
         mMapView = (MapView) view.findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                MyLocationConfiguration.LocationMode.COMPASS, true, null));
     }
 
     @Override
     public void initListener() {
-
+        // 初始化搜索模块，注册事件监听
+        mSearch = GeoCoder.newInstance();
+        mSearch.setOnGetGeoCodeResultListener(this);
+        showLocationOnMap(null);
     }
 
     @Override
     public void initData() {
-
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        // 定位初始化
+        mLocClient = new LocationClient(context);
+        mLocClient.registerLocationListener(new MyLocationListenner(mMapView,mBaiduMap));
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
     }
 
     @Override
@@ -58,5 +91,48 @@ public class TrrainFragment extends MVPBaseFragment<TrrainContract.View, TrrainP
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+    }
+
+    @Override
+    public void onGetGeoCodeResult(GeoCodeResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(context, "抱歉，未能找到结果", Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        mBaiduMap.clear();
+        mBaiduMap.addOverlay(new MarkerOptions().position(result.getLocation())
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.mipmap.icon_gcoding)));
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(result
+                .getLocation()));
+        String strInfo = String.format("纬度：%f 经度：%f",
+                result.getLocation().latitude, result.getLocation().longitude);
+        Toast.makeText(context, strInfo, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(context, "抱歉，未能找到结果", Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        mBaiduMap.clear();
+        mBaiduMap.addOverlay(new MarkerOptions().position(result.getLocation())
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.mipmap.icon_gcoding)));
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(result
+                .getLocation()));
+        Toast.makeText(context, result.getAddress(),
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    public void  showLocationOnMap( LatLng ptCenter){
+        // 反Geo搜索
+        ptCenter = new LatLng(113.666521, 34.753978);
+        mSearch.reverseGeoCode(new ReverseGeoCodeOption()
+                .location(ptCenter));
     }
 }
