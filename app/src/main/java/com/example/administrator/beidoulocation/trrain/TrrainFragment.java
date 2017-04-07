2ac69acdd11f466bee333ff7867fc2cd21949ac7
ake <7473276@qq.com>
2017-04-07 21:50:21 +0800

    添加3d地图

--- app/src/main/java/com/example/administrator/beidoulocation/trrain/TrrainFragment.java
+++ app/src/main/java/com/example/administrator/beidoulocation/trrain/TrrainFragment.java
@@ -3,6 +3,9 @@ package com.example.administrator.beidoulocation.trrain;
 
 import android.view.View;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.example.administrator.beidoulocation.R;
 import com.example.administrator.beidoulocation.mvp.MVPBaseFragment;
 
 /**
@@ -13,14 +16,19 @@ import com.example.administrator.beidoulocation.mvp.MVPBaseFragment;
 public class TrrainFragment extends MVPBaseFragment<TrrainContract.View, TrrainPresenter>
         implements TrrainContract.View {
 
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+
     @Override
-    public int getLayoutResId() {
-        return 0;
+   public int getLayoutResId() {
+        return R.layout.terrain_fragment;
     }
 
     @Override
     public void initView(View view) {
-
+        mMapView = (MapView) view.findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
     }
 
     @Override
@@ -31,5 +39,24 @@ public class TrrainFragment extends MVPBaseFragment<TrrainContract.View, TrrainP
     @Override
     public void initData() {
 
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
