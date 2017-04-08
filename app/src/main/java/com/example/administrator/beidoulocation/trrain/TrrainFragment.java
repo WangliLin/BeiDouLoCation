2b521197a90ea638731a7600f05d6ddd2ee3d1fa
liuting <122724021@qq.com>
2017-04-08 17:18:47 +0800

    添加key

--- app/src/main/java/com/example/administrator/beidoulocation/trrain/TrrainFragment.java
+++ app/src/main/java/com/example/administrator/beidoulocation/trrain/TrrainFragment.java
@@ -25,7 +25,7 @@ import com.example.administrator.beidoulocation.mvp.MVPBaseFragment;
 
 /**
  * MVPPlugin
- *  邮箱 784787081@qq.com
+ *  
  */
 
 public class TrrainFragment extends MVPBaseFragment<TrrainContract.View, TrrainPresenter>
